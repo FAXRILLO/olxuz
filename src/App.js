fcbde34ac374262330301bea5ee73a1514a60ea9
sunnatp <xabibullayevsunnat9@gmail.com>
2024-04-05 15:15:57 +0500

    routerlar ornatildi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Footer from './pages/Footer/Footer';
+import Home from './pages/Home/Home';
+import Navbar from './pages/Navbar/Navbar';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar />
+      <Home />
+        <Routes>
+          {/* <Route path='/' element={<About /> } /> */}
+          
+        </Routes>
+      <Footer />
+    </div>
   )
 }
 
