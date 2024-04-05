55e1bf941f0bf845a3a9922eae897e4aeda1c59b
sunnatp <xabibullayevsunnat9@gmail.com>
2024-04-05 15:32:45 +0500

    context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { InfoProvider, useInfoContext } from './context/Context';
 import Footer from './pages/Footer/Footer';
 import Home from './pages/Home/Home';
 import Navbar from './pages/Navbar/Navbar';
 
 const App = () => {
+  const {currentUser} = useInfoContext()
+  console.log(currentUser);
   return (
     <div className='app'>
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <Home />
         <Routes>
           {/* <Route path='/' element={<About /> } /> */}
-          
+
         </Routes>
-      <Footer />
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { InfoProvider } from './context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <InfoProvider>
         <App />
-    </BrowserRouter>
+    </InfoProvider>
 );
 
