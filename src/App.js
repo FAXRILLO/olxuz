cc20d56cbd9027cbd5d955c3f4e9035dc496316a
sunnatp <xabibullayevsunnat9@gmail.com>
2024-04-08 16:20:52 +0500

    hedr

--- src/App.js
+++ src/App.js
@@ -2,33 +2,23 @@ import React from "react";
 import { useInfoContext } from "./context/Context";
 import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./pages/Navbar/Navbar";
+import Footer from "./pages/Footer/Footer";
 
 const App = () => {
   const { currentUser } = useInfoContext();
   console.log(currentUser);
   return (
-<<<<<<< HEAD
     <Router>
       <div className="app">
+       <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
 };
-=======
-    <div className='app'>
-      <BrowserRouter>
-        <Navbar />
-          <Routes>
-            <Route path='/' element={<Home /> } />
-          </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  )
-}
->>>>>>> ab0f254dd35ea662c53dd7cdf7d8c608d4bc4731
 
 export default App;
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
     <div>
-      Navbar
+      <nav className = "navbar">
+        <div className = "container-fluid">
+          <h1 className = "brand">olx</h1>
+          <form className = "d-flex">
+            <h5 className='xabar'><i style={{marginRight: "10px"}} className ="fa-regular fa-comment"></i> Xabarlar</h5>
+            <h5 className='til'>O'Z | PY</h5>
+            <h5><i className ="icon fa-sharp fa-regular fa-heart"></i></h5>
+            <h5 className='hisoplar'><i style={{marginRight: "7px"}} className ="fa-regular fa-user"></i> Hisobingiz</h5>
+            <button className = "button" type="submit">E'lon berish</button>
+          </form>
+        </div>
+      </nav>
     </div>
   )
 }
