3fb57b6cdcdb2d3306f1cddd491435d8db064d76
FAXRILLO <faxrillo67@gmail.com>
2024-04-08 16:19:27 +0600

    style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useInfoContext } from "./context/Context";
 import Home from "./pages/Home/Home";
+import SignUp from "./pages/SignUp/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <div className="app">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </div>
     </Router>
--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./Signup.scss"
 
 const Signup = () => {
   return (
-    <div>
-      Signup
+    <div className='signup'>
+      <div className="signup-page">
+        <button className="facebook">
+        <i class="fa-brands fa-facebook"></i>
+        <span>Продолжить через Facebook</span>
+        </button>
+      </div>
     </div>
   )
 }
