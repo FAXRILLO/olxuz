332c9e0ddff5e466f4ac27b7b118b588c8c22ebd
FAXRILLO <faxrillo67@gmail.com>
2024-04-15 18:35:15 +0600

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import { useInfoContext } from "./context/Context";
 import Home from "./pages/Home/Home";
 import SignUp from "./pages/SignUp/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./pages/Navbar/Navbar";
-import Footer from "./pages/Footer/Footer";
+import { ToastContainer } from "react-toastify";
+
 
 const App = () => {
   const { currentUser } = useInfoContext();
-  console.log(currentUser, 4);
   return (
     <Router>
       <div className="app">
         <Routes>
 
-        {/* <Route path="/" element={currentUser ? <Home /> : <SignUp />} /> */}
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={currentUser ? <Home /> : <SignUp />} />  
+          
         </Routes>
+        <ToastContainer />
+
       </div>
     </Router>
   );
