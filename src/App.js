9659a2de2f7fa6cac14e55466926fac665366a02
FAXRILLO <faxrillo67@gmail.com>
2024-04-17 20:47:09 +0600

    update

--- src/App.js
+++ src/App.js
@@ -18,14 +18,12 @@ const App = () => {
   return (
     <Router>
       <div className="app">
-        <Navbar />
         <Routes>
           <Route path="/" element={currentUser ? <Home /> : <SignUp />} />
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/oneinfo" element={<OneInfo />} /> */}
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/oneinfo" element={<OneInfo />} />
           
         </Routes>
-        <Footer />
 
         <ToastContainer />
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,20 @@ import Serch from '../Serch/Serch'
 import Menyu from '../Menyu/Menyu'
 import HomeData from '../HomeData/HomeData'
 import Footer1 from '../Footer/Footer1'
+import Footer from '../Footer/Footer'
+import Navbar from '../Navbar/Navbar'
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
+
       <Serch />
       <Menyu />
       <HomeData />
       <Footer1 />
+      <Footer />
+
     </div>
   )
 }
